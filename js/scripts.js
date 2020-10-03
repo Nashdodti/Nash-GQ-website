7c1bc8d5c26344d59514b2b501f02b8ba68ae07b
Nashdodti <41152814+Nashdodti@users.noreply.github.com>
2020-10-03 13:24:21 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@
 		// TYPEWRITER
 			$("#typewriter").typewriter({
 				prefix : "",
-				text : ["Please wait", "Still loading", "Almost done"],
+				text : ["Keep Rolling", "Still loading", "Almost done"],
 				typeDelay : 100,
 				waitingTime : 1500,
 				blinkSpeed : 800
