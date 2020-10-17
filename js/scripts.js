1cd7a135a4e55b08c8fea09e29049593122b790d
Nashdodti <41152814+Nashdodti@users.noreply.github.com>
2020-10-17 17:47:05 +0530

    c

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@
 		// TYPEWRITER
 			$("#typewriter").typewriter({
 				prefix : "",
-				text : ["Keep Rolling", "Lets GO"],
+				text : ["L O A D I N G"],
 				typeDelay : 100,
 				waitingTime : 1500,
 				blinkSpeed : 800
